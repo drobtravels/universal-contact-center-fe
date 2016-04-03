ccd53a6f91d1ccc99fb014c447f51991ecc6fc39
David Roberts <drob@drob.net>
2016-04-03 15:51:39 -0400

    Add fetch polyfill

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,7 +15,10 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    })
   ],
   module: {
     loaders: [{
