83b46ebf3054eb1386e74256cecd73f171d019a1
David Roberts <drob@drob.net>
2016-05-14 17:57:23 -0400

    Moved out reply form to generic WithReplyForm HoC.  This will allow the reply functionality to be re-used for e-mails and SMS

--- src/SmsMessageTask.js
+++ src/SmsMessageTask.js
@@ -1,48 +1,14 @@
-import React, { Component } from 'react';
-import { FormGroup, ControlLabel, FormControl, Glyphicon, Button, ButtonGroup } from 'react-bootstrap';
+import React, { Component } from 'react'
+import { WithReplyForm } from './WithReplyForm'
+import { FormGroup, ControlLabel, FormControl, Glyphicon, Button, ButtonGroup } from 'react-bootstrap'
 
-class ReplyForm extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { replyText: '' }
-  }
-
-  setText = (event) => {
-    this.setState({ replyText: event.target.value })
-  }
-
-  onSend = () => {
-    this.props.completeTask({ reply: this.state.replyText })
-  }
-
-  render() {
-    return(<div>
-      <FormGroup>
-        <ControlLabel>Reply</ControlLabel>
-        <FormControl componentClass="textarea" onChange={this.setText} />
-      </FormGroup>
-      <Button onClick={this.onSend}>
-        <Glyphicon glyph="send"> Send & Complete</Glyphicon>
-      </Button>
-    </div>)
-  }
-}
-
-export class SmsMessageTask extends Component {
+class SmsMessageTaskClass extends Component {
   static propTypes = {
     From: React.PropTypes.string.isRequired,
     message: React.PropTypes.string.isRequired,
-    completeTask: React.PropTypes.func.isRequired
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = { showReplyForm: false }
-  }
-
-  onToggleReply = () => {
-    this.setState({ showReplyForm: !this.state.showReplyForm })
+    completeTask: React.PropTypes.func.isRequired,
+    replyButton: React.PropTypes.element.isRequired,
+    replyForm: React.PropTypes.element
   }
 
   onCall = () => {
@@ -50,13 +16,6 @@ export class SmsMessageTask extends Component {
   }
 
   render() {
-    var replyForm
-    var replyToggleText = 'Reply'
-    if (this.state.showReplyForm) {
-      replyForm = <ReplyForm completeTask={this.props.completeTask} />
-      replyToggleText = 'Hide'
-    }
-
     return(<form>
       <FormGroup>
         <ControlLabel>From</ControlLabel>
@@ -70,14 +29,14 @@ export class SmsMessageTask extends Component {
           readOnly />
       </FormGroup>
       <ButtonGroup>
-        <Button bsStyle="primary" onClick={this.onToggleReply}>
-          <Glyphicon glyph="comment"> {replyToggleText}</Glyphicon>
-        </Button>
+        {this.props.replyButton}
         <Button bsStyle="info" onClick={this.onCall}>
           <Glyphicon glyph="earphone"> Call</Glyphicon>
         </Button>
       </ButtonGroup>
-      { replyForm }
+      { this.props.replyForm }
     </form>)
   }
 }
+
+export var SmsMessageTask = WithReplyForm(SmsMessageTaskClass)
---
+++ src/WithReplyForm.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { FormGroup, ControlLabel, FormControl, Button, Glyphicon } from 'react-bootstrap'
+
+class ReplyForm extends Component {
+  static propTypes = {
+    completeTask: React.PropTypes.func.isRequired
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { replyText: '' }
+  }
+
+  setText = (event) => {
+    this.setState({ replyText: event.target.value })
+  }
+
+  onSend = () => {
+    this.props.completeTask({ reply: this.state.replyText })
+  }
+
+  render() {
+    return(<div>
+      <FormGroup>
+        <ControlLabel>Reply</ControlLabel>
+        <FormControl componentClass="textarea" onChange={this.setText} />
+      </FormGroup>
+      <Button onClick={this.onSend}>
+        <Glyphicon glyph="send"> Send & Complete</Glyphicon>
+      </Button>
+    </div>)
+  }
+}
+
+let ReplyFormWrapper = (props) => {
+  if(props.show) {
+    console.log('reply form...')
+    return(<ReplyForm {...props}/>)
+  } else {
+    console.log('return null...')
+    return(null)
+  }
+}
+
+let ReplyButton = (props) => {
+  return(
+    <Button onClick={props.onClick} bsStyle="primary">
+      <Glyphicon glyph="comment">
+        { props.show ? ' Hide' : ' Reply' }
+      </Glyphicon>
+    </Button>
+  )
+}
+
+export var WithReplyForm = (ComposedComponent) => class extends Component {
+  static displayName = 'WithReplyFormWrapper'
+  static propTypes = {
+    completeTask: React.PropTypes.func.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { showReplyForm: false }
+  }
+
+  onToggle = () => {
+    this.setState({ showReplyForm: !this.state.showReplyForm })
+  }
+
+  render() {
+    return(
+      <ComposedComponent {...this.props}
+        replyButton={<ReplyButton onClick={this.onToggle} show={this.state.showReplyForm} />}
+        replyForm={<ReplyFormWrapper show={this.state.showReplyForm} {...this.props}/>}
+        />
+    )
+  }
+}
