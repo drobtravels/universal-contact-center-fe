79b66084dd419a2f5aaa1751f5596f4d8804148d
David Roberts <drob@drob.net>
2016-05-23 10:13:40 -0700

    one other case correction

--- src/ConnectToTwilio.js
+++ src/ConnectToTwilio.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'React';
+import React, { Component } from 'react';
 import { API } from './api';
 
 export var ConnectToTwilio = ComposedComponent => class extends Component {
